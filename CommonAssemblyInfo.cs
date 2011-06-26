c4d985ec99877f942beea147f895e7dffc41e2f6
Richard Nienaber <rjnienaber@gmail.com>
2011-06-26 13:46:03 +0100

    Added common assembly info for version metadata

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyFileVersion("0.1")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: ComVisible(false)]
